4fc58dea2b8a3e799a77b61bc42dabc86564a92a
Harapapa <aduss1700@gmail.com>
2023-10-24 14:38:53 +0200

    1

--- Gyakszi/Javascript.js
+++
@@ -1,7 +0,0 @@
-const today = new Date();
-const day = today.getDate();
-const month = today.getMonth() + 1; // January is 0!
-const year = today.getFullYear();
-
-const todayDate = `${year}-${month}-${day}`;
-console.log(todayDate);
